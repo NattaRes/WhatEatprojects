287ab5e4a9157f5de1ca5b9bad8a5ffced486697
Guideus <GGWPTH@gmail.com>
2022-01-23 21:02:31 +0700

    Details page (incomplete)

--- app/src/main/java/com/example/whateatprojects/listdetail.java
+++ app/src/main/java/com/example/whateatprojects/listdetail.java
@@ -3,12 +3,23 @@ package com.example.whateatprojects;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class listdetail extends AppCompatActivity {
+
+    TextView textView;
+
+    String getresID = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listdetail);
+
+        getresID = getIntent().getStringExtra("ResID");
+
+        textView = (TextView) findViewById(R.id.nameres);
+
+        textView.setText(getresID);
     }
 }
